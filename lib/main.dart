b2c37aaacd708053a0b365903b5d7f53685fbe17
Meenakshi Bharadwaj <minakshi29bharadwaj@gmail.com>
2023-04-10 22:30:57 +0530

    added statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,15 +10,72 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: MyDicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+/*class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Image.asset('images/dice1.png'),
+          Image.asset('images/dice1.png'),
+        ],
+      ),
+    );
   }
 }
+*/
+class MyDicePage extends StatefulWidget {
+  const MyDicePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyDicePage> createState() => _MyDicePageState();
+}
+
+class _MyDicePageState extends State<MyDicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void rollDicee() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                rollDicee();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/* 
+TextButton(
+  onPressed: () {
+    setState(() {
+      diceNumber = Random().nextInt(6) + 1;
+    });
+  },
+  Widget: Image(
+    image: AssetImage('images/$diceNumber.png'),
+  ),
+),
+*/
